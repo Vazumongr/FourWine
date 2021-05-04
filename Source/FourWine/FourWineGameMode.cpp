4341c485ba93d4cbfb9947664a96664597dbb839
Troy Records Jr <giants2110@gmail.com>
2021-05-04 15:53:44 -0400

    Added Hammer Weapon Type
    Just another weapon type with different abilities. Can swap between freely.

--- Source/FourWine/FourWineGameMode.cpp
+++ Source/FourWine/FourWineGameMode.cpp
@@ -6,10 +6,5 @@
 
 AFourWineGameMode::AFourWineGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_PlayerCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	
 }
